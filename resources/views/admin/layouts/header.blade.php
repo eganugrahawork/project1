7998635b2c15fca17a6e48229b5a3fd28a646378
eganugrahawork <eganugrahawork@gmail.com>
2022-10-07 07:51:09 +0700

    header cssnya hover dsb

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -3,7 +3,8 @@
     <!--begin::Container-->
     <div class="container-xxl d-flex flex-grow-1 flex-stack">
         <div class="d-flex align-items-center me-5">
-            <div class="d-lg-none btn btn-icon btn-active-color-primary w-30px h-30px ms-n2 me-3" id="kt_header_menu_toggle">
+            <img alt="Logo" src="{{ asset('storage/logos/loccanalogo.png') }}" class="h-30px h-lg-40px" />
+            <div class="d-lg-none btn btn-icon btn-active-color-primary w-30px h-30px ms-2 me-3" id="kt_header_menu_toggle">
                 <span class="svg-icon svg-icon-1">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                         <path d="M21 7H3C2.4 7 2 6.6 2 6V4C2 3.4 2.4 3 3 3H21C21.6 3 22 3.4 22 4V6C22 6.6 21.6 7 21 7Z" fill="black" />
@@ -12,27 +13,10 @@
                 </span>
                 <!--end::Svg Icon-->
             </div>
-            <img alt="Logo" src="{{ asset('storage/logos/loccanalogo.png') }}" class="h-30px h-lg-40px" />
         </div>
         <div class="d-flex align-items-center">
             <!--begin::Topbar-->
             <div class="d-flex align-items-center flex-shrink-0">
-                <!--begin::Search-->
-                <div id="kt_header_search" class="d-flex align-items-center w-lg-225px" data-kt-search-keypress="true" data-kt-search-min-length="2" data-kt-search-enter="enter" data-kt-search-layout="menu" data-kt-search-responsive="lg" data-kt-menu-trigger="auto" data-kt-menu-permanent="true" data-kt-menu-placement="bottom-end">
-                    <!--begin::Tablet and mobile search toggle-->
-                    <div data-kt-search-element="toggle" class="d-flex d-lg-none align-items-center">
-                        <div class="btn btn-icon btn-color-gray-700 btn-active-color-primary btn-outline btn-outline-secondary w-30px h-30px">
-                            <!--begin::Svg Icon | path: icons/duotune/general/gen004.svg-->
-                            <span class="svg-icon svg-icon-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <path d="M21.7 18.9L18.6 15.8C17.9 16.9 16.9 17.9 15.8 18.6L18.9 21.7C19.3 22.1 19.9 22.1 20.3 21.7L21.7 20.3C22.1 19.9 22.1 19.3 21.7 18.9Z" fill="black" />
-                                    <path opacity="0.3" d="M11 20C6 20 2 16 2 11C2 6 6 2 11 2C16 2 20 6 20 11C20 16 16 20 11 20ZM11 4C7.1 4 4 7.1 4 11C4 14.9 7.1 18 11 18C14.9 18 18 14.9 18 11C18 7.1 14.9 4 11 4ZM8 11C8 9.3 9.3 8 11 8C11.6 8 12 7.6 12 7C12 6.4 11.6 6 11 6C8.2 6 6 8.2 6 11C6 11.6 6.4 12 7 12C7.6 12 8 11.6 8 11Z" fill="black" />
-                                </svg>
-                            </span>
-                            <!--end::Svg Icon-->
-                        </div>
-                    </div>
-                </div>
                 <div class="d-flex align-items-center ms-3 ms-lg-4" id="kt_header_user_menu_toggle">
                     <div class="btn btn-icon btn-color-gray-700 btn-active-color-primary btn-outline btn-outline-secondary w-30px h-30px w-lg-40px h-lg-40px" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
                         <span class="svg-icon svg-icon-1">
@@ -116,7 +100,7 @@
                         @endphp
                             <div data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start" class="menu-item {{ $check == true ? 'here show' : ''}} menu-lg-down-accordion me-lg-1">
                                 <span class="menu-link  py-3">
-                                    <span class="menu-title text-white">{{ $m->menu }}</span>
+                                    <span class="menu-title">{{ $m->menu }}</span>
                                     <span class="menu-arrow d-lg-none"></span>
                                 </span>
                                 <div class="menu-sub menu-sub-lg-down-accordion menu-sub-lg-dropdown menu-rounded-0 py-lg-4 w-lg-225px">
@@ -137,7 +121,7 @@
                                         </span>
                                         <!--end::Svg Icon-->
                                     </span>
-                                    <span class="menu-title">{{ $sub->submenu }}</span>
+                                    <span class="menu-title text-gray-700">{{ $sub->submenu }}</span>
                                 </a>
                             </div>
                             @endif
@@ -149,8 +133,8 @@
                         $str = substr($m->url, 1);
                     @endphp
                         <div  class="menu-item   me-lg-1">
-                            <a class="menu-link {{ Request::is($str) ? 'active' : ''}} py-3" href="{{ $m->url }}">
-                                <span class="menu-title text-white">{{ $m->menu }}</span>
+                            <a class="menu-link  {{ Request::is($str) ? 'active' : ''}} py-3" data-bs-toggle="tooltip" data-bs-trigger="hover" href="{{ $m->url }}">
+                                <span class="menu-title">{{ $m->menu }}</span>
                                 <span class="menu-arrow d-lg-none"></span>
                             </a>
                         </div>
