99aacca69335b8851566044aa7e383166ae5aa15
eganugrahawork <eganugrahawork@gmail.com>
2022-11-30 14:45:13 +0700

    landing page

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function () {
-    return view('auth.login');
+    return view('welcome');
 })->middleware('guest');
 
 Route::middleware(['guest'])->controller(RegisterController::class)->group(function(){
